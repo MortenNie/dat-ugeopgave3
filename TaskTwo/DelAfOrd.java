01eb4cf6979a594e6c9b520d8a0ec528c2feb30a
MortenNie <cph-mn705@cphbusiness.dk>
2022-09-22 13:11:07 +0200

    move files

---
+++ TaskTwo/DelAfOrd.java
@@ -0,0 +1,13 @@
+public class DelAfOrd {
+	
+public void printPartOfWord(String word, int r, int p) {
+	int m = r + p;
+	String t = word.substring(r, m);
+	System.out.println(t);
+
+}
+
+
+}
+
+
---
+++ TaskTwo/Main2.java
@@ -0,0 +1,7 @@
+public class Main2 {
+	public static void main(String[] args) {
+	DelAfOrd h = new DelAfOrd();
+    h.printPartOfWord("Kobenhavn", 1, 4);
+
+	}
+}
